5fb37fea03f60d9010b710c6301a38dd86c17628
blowfishlol <wibisonoyulius@gmail.com>
2020-10-14 01:29:00 +0700

    Initial Commit - React project with create-react-app, able to connect to backend side with socket.io

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import io from "socket.io-client"
 import logo from './logo.svg';
 import './App.css';
 
+const socket = io("localhost:5000")
+
 function App() {
+
+  const [chatArray, setChatArray] = useState([])
+
+
+  useEffect(() =>{
+
+    socket.on("response" , data =>{
+      console.log(data)
+      setChatArray([...setChatArray, data])
+    })
+
+    return () => socket.disconnect()
+  })
+
+  const handleSubmit = (message) =>{
+    socket.emit("message", message)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
